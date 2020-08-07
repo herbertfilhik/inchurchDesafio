0a949d2a2c6934e861fc5e4445d2ddea9f8b8ae6
herbert <herbert@herberts-MacBook-Pro.local>
2020-08-07 12:37:28 -0300

    readme atualizacao

--- README.md
+++ README.md
@@ -1,7 +1,83 @@
 # inchurch challenge
 
 ## Desafio:
-1. O nosso time de desenvolvimento está trabalhando em uma nova feature, que foi alinhada hoje na nossa reunião de planejamento. A feature se resume em, o usuário deseja realizar a inscrição em um evento pelo nosso site, onde ele tem a possibilidade de realizar um login, fazer a inscrição selecionando a quantidade de tickets, preenchendo informações adicionais como nome, telefone e cpf (campos obrigatórios) e a realização de pagamento do ticket via cartão de crédito e boleto. Dado o cenário, é necessário realizar a descrição de possíveis casos de testes.
-2. Com a possível feature entregue pelo time de desenvolvimento, e em fase de validação, desenvolva testes automatizados para a realização da validação. Link da feature: ​https://qa-suaigreja.inchurch.com.br/#/events/2574
+1. O nosso time de desenvolvimento está trabalhando em uma nova feature, 
+que foi alinhada hoje na nossa reunião de planejamento. A feature se resume em, 
+o usuário deseja realizar a inscrição em um evento pelo nosso site, 
+onde ele tem a possibilidade de realizar um login, fazer a inscrição selecionando a quantidade de tickets, 
+preenchendo informações adicionais como nome, telefone e cpf (campos obrigatórios)
+e a realização de pagamento do ticket via cartão de crédito e boleto. 
+Dado o cenário, é necessário realizar a descrição de possíveis casos de testes.
+
+Funcionalidade: Realizar Inscrição para Eventos da inchurch
+
+Cenário: 01 - Criar login para o cliente realizar inscrição
+	Dado que o que cliente acesse o site para inscrição
+	E realize o cadastro no site
+		|ticket     |nome       |fone        |cpf        |
+		|1	        |alvaro dias|11-4444-2222|78534769818|
+		|0	        |alvaro dias|11-4444-2222|78534769818|
+		|1	        |!@#$%ˆ&*()_|11-4444-2222|78534769818|
+		|9999       |alvaro dias|11-4444-2222|78534769818|
+		|1	        |alvaro dias|!@#$%ˆ&*()__|78534769818|
+		|1	        |alvaro dias|11-4444-2222|11122233344|
+		|1 	        |alvaro dias|11-4444-2222|!@#$%ˆ&*()!|
+		|10 	    |alvaro dias|11-4444-2222|68484027880|
+	E realiza validacao dos dados para efetivar o cadastro do cliente															
+	Entao deve ser gerado um login para o cliente
+
+
+Cenário: 02 - Realizar login
+	Dado que o cliente esteja cadastrado
+	E esteja no site
+	E informa usuario e senha
+	Entao deve acessar o sistema
+
+Cenario: 03 - Tentar realizar login com usuario invalido ou inexistente
+	Dados que nao exista o cliente na base
+	E esteja no site
+	E informa usuario e senha invalidos ou inexistentes
+	Entao o sistema devera informar que o usuario eh inexistente ou invalido
+	
+Cenario: 04 - Realizar pagamento para o evento com cartao de credito
+	Dado que o cliente esteja cadastrado na base
+	E esteja no site
+	E realiza o login
+	E esteja no carrinho de compras
+	E seleciona o meio de pagamento CARTAO
+	E informa os dados de pagamento
+		|cartao          |bandeira|dtexpiracao|cvv|nome           |
+		|4556302487225856|visa    |09/21      |907|antonieta souza|
+		|5330991798089462|master  |07/21      |765|jose santos    |
+		|xxxxxxxxxxxxxxxx|error   |xx/ss      |123|!@#$%ˆ&*((     |
+	E valida se o meio de pagamento eh valido
+	Entao realiza pagamento e retorna voucher para o cliente
+	
+Cenario: 05 - Realizar pagamento para o evento com boleto
+	Dado que o cliente esteja cadastrado na base
+	E esteja no site
+	E realiza o login
+	E esteja no carrinho de compras
+	E seleciona o meio de pagamento BOLETO
+	Entao o sistema deve gerar o boleto para pagamento e disponibilizar ao cliente
+		|boleto            |dataVencimento              |
+		|valido            |dentro da data de vencimento|
+		|valido            |fora da data de vencimento  |
+		|invalido          |dentro da data de vencimento|
+		|valido segunda via|dentro da data de vencimento|		
+		|valido segunda via|fora da data de vencimento  |				
+	Entao o cliente deve realizar o pagamento
+	Entao o sistema valida se o pagamento foi realizado de acordo com as politicas financeiras da empresa
+	Entao se o pagamento foi realizado com sucesso o sistema disponibiliza o voucher ao cliente
+	
+	
+-----
+	
+
+2. Com a possível feature entregue pelo time de desenvolvimento, e em fase de validação, 
+desenvolva testes automatizados para a realização da validação. 
+Link da feature: ​https://qa-suaigreja.inchurch.com.br/#/events/2574
 Login: desafio@qa.inchurch.com
 Senha: qainchurch
+
+DISPONIBILIZADO NESTE REPOSITORIO NO GITHUB
--- target/htmlreports/report.js
+++ target/htmlreports/report.js
@@ -7,11 +7,11 @@ formatter.feature({
   "keyword": "Feature"
 });
 formatter.before({
-  "duration": 367746859,
+  "duration": 326045129,
   "status": "passed"
 });
 formatter.before({
-  "duration": 53844,
+  "duration": 43805,
   "status": "passed"
 });
 formatter.scenario({
@@ -41,29 +41,29 @@ formatter.match({
   "location": "LoginSteps.i_am_on_inchurch_site()"
 });
 formatter.result({
-  "duration": 12861489811,
+  "duration": 7027373644,
   "status": "passed"
 });
 formatter.match({
   "location": "LoginSteps.i_search_for_Entrar_como_visitante()"
 });
 formatter.result({
-  "duration": 6590520275,
-  "status": "passed"
+  "duration": 79260041,
+  "error_message": "org.openqa.selenium.WebDriverException: TypeError: rect is undefined\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027herberts-MacBook-Pro.local\u0027, ip: \u00272804:431:c7e0:6c73:1d00:5a67:9ac0:e885%en0\u0027, os.name: \u0027Mac OS X\u0027, os.arch: \u0027x86_64\u0027, os.version: \u002710.15.5\u0027, java.version: \u00271.8.0_211\u0027\nDriver info: org.openqa.selenium.firefox.FirefoxDriver\nCapabilities {acceptInsecureCerts: true, browserName: firefox, browserVersion: 79.0, javascriptEnabled: true, moz:accessibilityChecks: false, moz:buildID: 20200720193547, moz:geckodriverVersion: 0.26.0, moz:headless: false, moz:processID: 28100, moz:profile: /var/folders/7t/pcfjnxqn27s..., moz:shutdownTimeout: 60000, moz:useNonSpecCompliantPointerOrigin: false, moz:webdriverClick: true, pageLoadStrategy: normal, platform: MAC, platformName: MAC, platformVersion: 19.5.0, rotatable: false, setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify}\nSession ID: 804d1e33-8ab6-874a-b841-65c0c8017aa9\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\n\tat org.openqa.selenium.remote.RemoteWebDriver.perform(RemoteWebDriver.java:618)\n\tat org.openqa.selenium.interactions.Actions$BuiltAction.perform(Actions.java:639)\n\tat org.openqa.selenium.interactions.Actions.perform(Actions.java:595)\n\tat stepdefinitions.inchurch.LoginSteps.i_search_for_Entrar_como_visitante(LoginSteps.java:43)\n\tat ✽.When I search for Entrar como visitante(login.feature:5)\n",
+  "status": "failed"
 });
 formatter.match({
   "location": "LoginSteps.i_should_make_login_on_system()"
 });
 formatter.result({
-  "duration": 28559773802,
+  "status": "skipped"
+});
+formatter.after({
+  "duration": 4428850256,
   "status": "passed"
 });
 formatter.after({
-  "duration": 2457828836,
-  "status": "passed"
-});
-formatter.after({
-  "duration": 27151,
+  "duration": 25823,
   "status": "passed"
 });
 formatter.uri("user.feature");
@@ -75,11 +75,11 @@ formatter.feature({
   "keyword": "Feature"
 });
 formatter.before({
-  "duration": 135090163,
+  "duration": 178866722,
   "status": "passed"
 });
 formatter.before({
-  "duration": 17689,
+  "duration": 17914,
   "status": "passed"
 });
 formatter.scenario({
@@ -109,29 +109,29 @@ formatter.match({
   "location": "UserSteps.i_am_on_inchurch_site()"
 });
 formatter.result({
-  "duration": 10886301801,
+  "duration": 10759508153,
   "status": "passed"
 });
 formatter.match({
   "location": "UserSteps.inputdatafornewuser()"
 });
 formatter.result({
-  "duration": 6485709897,
+  "duration": 6514257777,
   "status": "passed"
 });
 formatter.match({
   "location": "UserSteps.insertregisteronthebase()"
 });
 formatter.result({
-  "duration": 28832090778,
+  "duration": 28846297600,
   "status": "passed"
 });
 formatter.after({
-  "duration": 2440119649,
+  "duration": 581130992,
   "status": "passed"
 });
 formatter.after({
-  "duration": 19140,
+  "duration": 20549,
   "status": "passed"
 });
 });
